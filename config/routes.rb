07a9b23b70de1dccb9c071434d73bb5e66b715e8
yasuper8 <ysuper8@gmail.com>
2016-12-16 14:33:08 -0800

    set up user auth. login page. user page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/users" => "users#index", as: 'users'
+  # get "/users/new" => "users#new", as: 'new_user'
+  post "/users" => "users#create"
+
+  get '/' => 'sessions#index'
+  get '/login' => 'sessions#new', as: 'login'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new', as: 'new_user'
+
 end
